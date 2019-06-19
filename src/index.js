ce0603cb49db592c7ce1cbe19e648a817a5b7091
[Dorijan] <[dorijan11@gmail.com]>
2019-06-19 21:31:19 +0200

    created store and reducers folder and the actions js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// store constant is used to create a store that contains the entire application state
+const store = createStore(); 
+
+// We wrap our App with a Provider component that enables us to connect our React app to the store/ our redux state
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 registerServiceWorker();
