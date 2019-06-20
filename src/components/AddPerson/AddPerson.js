83421b35e674f1c593bebc0f82b2f5fe3c924bdf
[Dorijan] <[dorijan11@gmail.com]>
2019-06-20 16:58:46 +0200

    combined local ui state and redux

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,11 +1,55 @@
-import React from 'react';
+
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
+/*const addPerson = (props) => (
     <div className="AddPerson">
         <button onClick={props.personAdded}>Add Person</button>
     </div>
 );
 
-export default addPerson;
+*/
+
+
+import React, { Component } from 'react'
+
+ class AddPerson extends Component {
+
+    state={
+        name: '',
+        age: ''
+    }
+
+    nameChangedHandler = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    ageChangedHandler = (e) => {
+        this.setState({
+            age: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className='AddPerson'>
+                   <input type='text' 
+                   placeholder='Name' 
+                   onChange={this.nameChangedHandler}
+                   value={this.state.name}/>
+
+                   <input type='number'
+                    placeholder='Age' 
+                    onChange={this.ageChangedHandler}
+                    value={this.state.age}/>
+                   <button onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>  {/* Combining local UI state with the dispatcher */}
+            </div>
+        )
+    }
+}
+
+
+
+export default AddPerson;
--- src/store/reducers/persons.js
+++ src/store/reducers/persons.js
@@ -8,8 +8,8 @@ const reducer = (state = initialState, action) => {
   if (action.type === actionTypes.ADD_PERSON) {
     const newPerson = {
       id: Math.random(), // not really unique but good enough here!
-      name: "Max",
-      age: Math.floor(Math.random() * 40)
+      name: action.personInfo.name,
+      age: action.personInfo.age
     };
 
     return {
